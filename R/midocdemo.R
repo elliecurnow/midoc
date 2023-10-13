aa97d5ca99a017994f342aefe5fc4932a1d36256
elliecurnow <elinor.curnow@bristol.ac.uk>
2023-10-13 09:27:50 +0100

    Updated vignette

--- R/midocdemo.R
+++ R/midocdemo.R
@@ -1,10 +1,10 @@
 #' Run a Shiny demo of midoc
 #' @export
 midocdemo <- function() {
-  #appDir <- system.file("vignettes/midocdemoShiny.rmd", package = "midoc")
-  #if (appDir == "") {
-  #  stop("Could not find example directory. Try re-installing `midoc`.", call. = FALSE)
-  #}
+  appDir <- system.file("doc/midocdemoShiny.rmd", package = "midoc", mustWork=TRUE)
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `midoc`.", call. = FALSE)
+  }
 
-  rmarkdown::run("vignettes/midocdemoShiny.Rmd")
+  rmarkdown::run(appDir)
 }
