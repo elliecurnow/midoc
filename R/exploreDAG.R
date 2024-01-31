7b8be22164e725d973fdfc1b3e0c9081409321ca
elliecurnow <elinor.curnow@bristol.ac.uk>
2024-01-31 15:19:26 +0000

    Updated exploreDAG

--- R/exploreDAG.R
+++ R/exploreDAG.R
@@ -6,7 +6,8 @@
 #'
 #' @param mdag The DAG, specified as a string using \link[dagitty]{dagitty}
 #'   syntax
-#' @param data A data frame containing all the variables stated in the DAG
+#' @param data A data frame containing all the variables stated in the DAG. All
+#'   variables must be numeric.
 #'
 #' @return A message indicating whether the relationships between fully observed
 #'   variables in the specified dataset are consistent with the proposed DAG
@@ -29,7 +30,10 @@ exploreDAG <- function(mdag, data) {
   tests <- dagitty::impliedConditionalIndependencies(mod)
   comptests <- Filter(function(x) all(x$X %in% compvar) & all(x$Y %in% compvar) & all(x$Z %in% compvar),
                   tests)
-
+  comptestsres <- dagitty::localTests(x=mod,data=compdata,tests=comptests,type="cis",abbreviate.names=FALSE)
+  if(nrow(comptestsres)==0){
+    cat(strwrap("None of the fully observed variables are conditionally independent"))
+  }else{
   cat(strwrap("Method for exploring the relationships between fully observed variables given
 the proposed directed acyclic graph (DAG): linear conditional independence.
 See ??dagitty::localTests for further details."),"\n",fill=TRUE)
@@ -37,8 +41,9 @@ See ??dagitty::localTests for further details."),"\n",fill=TRUE)
   cat("\n",strwrap("A large p-value for a conditional model means there is little evidence of
 inconsistency between your data and the proposed DAG."),"\n",
 strwrap("A small p-value for a conditional model means your data may not be consistent
-with the proposed DAG and/or the model may be mis-specified.
-Check the specification of your DAG and each relationship in your model."),"\n",
+with the proposed DAG. Note that these results assume that relationships between variables are linear.
+Consider exploring the specification of your DAG and each relationship in your model."),"\n",
 strwrap("Consider also exploring relationships with partially observed variables
 using the observed data, if valid and possible, i.e. avoiding perfect prediction."),"\n",fill=TRUE)
+  }
 }
