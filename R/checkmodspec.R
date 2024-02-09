a8ef01a629e6e8b445b3b18974d60053886fdb24
elliecurnow <elinor.curnow@bristol.ac.uk>
2024-02-09 09:52:35 +0000

    Updated checkmodspec

--- R/checkmodspec.R
+++ R/checkmodspec.R
@@ -11,14 +11,18 @@
 #'   used in the model, specified as a string; family functions that are
 #'   supported are "gaussian(identity)" and "binomial(logit)"
 #' @param data A data frame containing all the variables stated in the formula
+#' @param message If TRUE, displays a message indicating whether the
+#'   relationships between the dependent variable and covariates are likely to
+#'   be correctly specified or not; use message = FALSE to suppress the message
 #' @param modplot If TRUE and there is evidence of model mis-specification,
 #'   displays a plot which can be used to explore the functional form of each
 #'   covariate in the specified model; use modplot = FALSE to disable the plot
 #'
-#' @return A message indicating whether the relationships between the dependent
-#'   variable and covariates are likely to be correctly specified or not, plus
-#'   an object of type 'miprop' (a list containing the specified formula,
-#'   family, and dataset name)
+#' @return An object of type 'miprop' (a list containing the
+#'   specified formula, family, and dataset name), plus, optionally, messages
+#'   and plots indicating whether the relationships between the dependent
+#'   variable and covariates are likely to be correctly specified or not
+#'
 #' @export
 #'
 #' @examples
@@ -29,49 +33,55 @@
 #' checkModSpec(formula="mated~bmi7+matage", family="binomial(logit)", data=bmi)
 #' checkModSpec(formula="mated~bmi7+matage+I(matage^2)",
 #'   family="binomial(logit)", data=bmi)
-checkModSpec <- function(formula, family, data, modplot=TRUE) {
+checkModSpec <- function(formula, family, data, message=TRUE, modplot=TRUE) {
 
   if(family == "gaussian(identity)"){
     mod <- stats::glm(stats::as.formula(formula), data=data)
     modfit <- data.frame(r=mod[["residuals"]],fitvals=mod[["fitted.values"]])
     modfittest <- mfp::mfp(r ~ fp(fitvals, df = 4, select=0.05), data=modfit)
     pval <- 1-stats::pchisq(modfittest$null.deviance-modfittest$deviance, modfittest$df.null-modfittest$df.residual)
-   cat(strwrap("Model mis-specification method: regression of model residuals on a fractional polynomial of the fitted values"),"\n",
+
+    if (message){
+      cat(strwrap("Model mis-specification method: regression of model residuals on a fractional polynomial of the fitted values"),"\n",
         "P-value:",
         pval,"\n",fill=TRUE)
-    if(pval > 0.1){
-      cat(strwrap("A large p-value means there is little evidence of model mis-specification."),"\n",fill=TRUE)
-    } else {
-      cat(strwrap("A small p-value means the model may be mis-specified.
-Check the specification of each relationship in your model."),"\n",fill=TRUE)
+      if(pval > 0.1){
+        cat(strwrap("A large p-value means there is little evidence of model mis-specification."),"\n",fill=TRUE)
+      } else {
+        cat(strwrap("A small p-value means the model may be mis-specified.
+        Check the specification of each relationship in your model."),"\n",fill=TRUE)
 
-      #Optionally create a plot of residual vs fitted values
-      if (modplot){
-        plot(modfit$fitvals,modfit$r,xlab="",ylab="",
-             main="Residuals versus fitted values",
-             sub=list("This plot may suggest the appropriate functional form \nfor the specified model",cex=0.8))
+        #Optionally create a plot of residual vs fitted values
+        if (modplot){
+          plot(modfit$fitvals,modfit$r,xlab="",ylab="",
+               main="Residuals versus fitted values",
+               sub=list("This plot may suggest the appropriate functional form \nfor the specified model",cex=0.8))
+        }
       }
     }
   }
+
   else if(family == "binomial(logit)"){
     mod <- stats::glm(formula=stats::as.formula(formula),family="binomial", data=data)
     modfittest <- blorr::blr_linktest(mod)
     pval <- stats::coef(modfittest)[3,4]
-    cat(strwrap("Model mis-specification method: Pregibon's link test"),"\n",
-        "P-value:",
-        pval,"\n", fill=TRUE)
-    if(pval > 0.1){
-      cat(strwrap("A large p-value means there is little evidence of model mis-specification."),"\n", fill=TRUE)
-    } else {
-      cat(strwrap("A small p-value means the model may be mis-specified.
-Check the specification of each relationship in your model."),"\n",fill=TRUE)
+    if (message){
+      cat(strwrap("Model mis-specification method: Pregibon's link test"),"\n",
+          "P-value:",
+          pval,"\n", fill=TRUE)
+      if(pval > 0.1){
+        cat(strwrap("A large p-value means there is little evidence of model mis-specification."),"\n", fill=TRUE)
+      } else {
+        cat(strwrap("A small p-value means the model may be mis-specified.
+        Check the specification of each relationship in your model."),"\n",fill=TRUE)
 
-      #Optionally create a plot of deviance residual vs binned fitted values
-      modfit <- data.frame(r=mod[["residuals"]],fitvals=mod[["fitted.values"]])
-      if (modplot){
-        arm::binnedplot(modfit$fitvals,modfit$r,xlab="",ylab="",col.int="white",
-                    main="Residuals versus (binned) fitted values",
-                    sub=list("This plot may suggest the appropriate functional form \nfor the specified model", cex=0.8))
+        #Optionally create a plot of deviance residual vs binned fitted values
+        modfit <- data.frame(r=mod[["residuals"]],fitvals=mod[["fitted.values"]])
+        if (modplot){
+          arm::binnedplot(modfit$fitvals,modfit$r,xlab="",ylab="",col.int="white",
+                      main="Residuals versus (binned) fitted values",
+                      sub=list("This plot may suggest the appropriate functional form \nfor the specified model", cex=0.8))
+        }
       }
     }
   }
