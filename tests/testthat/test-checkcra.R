818deb2038b86d24c939a075318227d9385469c3
elliecurnow <elinor.curnow@bristol.ac.uk>
2024-02-23 15:22:46 +0000

    Updated documentation

--- tests/testthat/test-checkcra.R
+++ tests/testthat/test-checkcra.R
@@ -2,7 +2,7 @@ res1<-evaluate_promise(checkCRA(y="edscore14", covs="iq8", r="r", mdag="iq8 -> e
 #There's a trailing blank, but only visible in testing, so just trim for test purposes
 test_that("checkCRA correctly identifies when CRA is valid given the mDAG and analysis model", {
   expect_equal(trimws(paste0(gsub("\n","",res1$output), collapse=" "),"right"),
-  "The analysis model outcome and complete record indicator are independent given analysis model covariates. Hence, complete records analysis is valid.")
+  "Based on the proposed directed acyclic graph (DAG), the analysis model outcome and complete record indicator are independent given analysis model covariates. Hence, complete records analysis is valid.")
   }
   )
 
--- tests/testthat/test-checkmi.R
+++ tests/testthat/test-checkmi.R
@@ -2,7 +2,7 @@ res1<-evaluate_promise(checkMI(dep="iq8", preds="edscore14 smoking", r="r", mdag
 #There's a trailing blank, but only visible in testing, so just trim for test purposes
 test_that("checkMI correctly identifies when MI is valid given the mDAG and imputation model", {
   expect_equal(trimws(paste0(gsub("\n","",res1$output), collapse=" "),"right"),
-"The incomplete variable and its missingness indicator are independent given imputation model predictors. Hence, multiple imputation methods which assume data are missing at random are valid in principle.")
+"Based on the proposed directed acyclic graph (DAG), the incomplete variable and its missingness indicator are independent given imputation model predictors. Hence, multiple imputation methods which assume data are missing at random are valid in principle.")
 })
 
 
