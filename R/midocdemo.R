ba53348a6073874e6fd735d30fe55c81efcf3a96
elliecurnow <elinor.curnow@bristol.ac.uk>
2024-02-26 14:41:23 +0000

    Renamed midocdemo function

--- R/midocdemo.R
+++ R/midocdemo.R
@@ -1,12 +1,12 @@
 #' Run an interactive vignette for the midoc package
 #'
 #' Runs an interactive version of the midoc vignette: Multiple Imputation DOCtor
-#' (midoc). In the interactive version, you can apply midoc functions using your
-#' own DAG and data.
+#' (midoc). In the interactive version, you can apply midoc functions in
+#' \link[shiny]{shiny-package} apps using your own DAG and data.
 #'
 #' @export
 #'
-midocdemo <- function() {
+runShinyVignette <- function() {
   appDir <- system.file("doc/midocdemoShiny.rmd", package = "midoc", mustWork=TRUE)
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `midoc`.", call. = FALSE)
