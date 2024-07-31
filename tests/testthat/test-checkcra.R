0bc86b9596c91303edece31d97013a96a92bc1e6
elliecurnow <elinor.curnow@bristol.ac.uk>
2024-07-31 12:01:39 +0100

    Updated website

--- tests/testthat/test-checkcra.R
+++ tests/testthat/test-checkcra.R
@@ -1,3 +1,4 @@
+#Check output when CRA is valid
 res1<-evaluate_promise(checkCRA(y="edscore14", covs="iq8", r="r", mdag="iq8 -> edscore14 iq8 -> r"))
 #There's a trailing blank, but only visible in testing, so just trim for test purposes
 test_that("checkCRA correctly identifies when CRA is valid given the mDAG and analysis model", {
@@ -5,5 +6,14 @@ test_that("checkCRA correctly identifies when CRA is valid given the mDAG and an
   "Based on the proposed directed acyclic graph (DAG), the analysis model outcome and complete record indicator are independent given analysis model covariates. Hence, complete records analysis is valid.")
   }
   )
+
+#Check output when CRA is not valid
+#res2<-evaluate_promise(checkCRA(y="edscore14", covs=" ", r="r", mdag="iq8 -> edscore14 iq8 -> r"))
+#There's a trailing blank, but only visible in testing, so just trim for test purposes
+#test_that("checkCRA correctly identifies when CRA is valid given the mDAG and analysis model", {
+#  expect_equal(trimws(paste0(gsub("\n","",res1$output), collapse=" "),"right"),
+#               "Based on the proposed directed acyclic graph (DAG), the analysis model outcome and complete record indicator are independent given analysis model covariates. Hence, complete records analysis is valid.")
+#}
+#)
 
 
