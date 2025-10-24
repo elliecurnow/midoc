0f28927e8b6ba7e02187deb63f4a9d27fd8e9f78
elliecurnow <elinor.curnow@bristol.ac.uk>
2025-10-24 14:09:35 +0100

    Updated exploreDAG to allow DAG input as a dagitty object, plus to give output based on DAG alone (plus option to supply data)

--- R/exploreDAG.R
+++ R/exploreDAG.R
@@ -5,13 +5,16 @@
 #' (DAG) using \link[dagitty]{localTests} functionality.
 #'
 #' @param mdag The DAG, specified as a string using \link[dagitty]{dagitty}
-#'   syntax
-#' @param data A data frame containing all the variables stated in the DAG. All
-#'   ordinal variables must be integer-coded and all categorical variables must
-#'   be dummy-coded.
+#'   syntax, or as a \link[dagitty]{dagitty} graph object
+#' @param data Optionally, a data frame containing all the variables stated in
+#'   the DAG. All ordinal variables must be integer-coded and all categorical
+#'   variables must be dummy-coded.
 #'
-#' @return A message indicating whether the relationships between fully observed
-#'   variables in the specified dataset are consistent with the proposed DAG
+#' @return A message listing the pairs of variables that are implied to be
+#'   independent (possibly conditional on other variables) by the proposed DAG.
+#'   Optionally, if a dataset is supplied, a message indicating whether the
+#'   relationships between fully observed variables in the specified dataset are
+#'   consistent with the proposed DAG.
 
 #' @export
 #'
@@ -19,30 +22,45 @@
 #' exploreDAG(mdag="matage -> bmi7 mated -> matage mated -> bmi7
 #'                  sep_unmeas -> mated sep_unmeas -> r",
 #'            data=bmi)
-exploreDAG <- function(mdag, data) {
-  mod <- dagitty::dagitty(paste('dag {',mdag,'}'), layout=T)
+exploreDAG <- function(mdag, data=NULL) {
 
-  # Keep fully observed variables from dataset
-  mdlist <- mice::md.pattern(data,plot=FALSE)
-  complist <- mdlist[nrow(mdlist),]
-  compvar <- names(complist[complist==0])
-  compdata <- data[,c(compvar)]
-
-  tests <- dagitty::impliedConditionalIndependencies(mod)
-  comptests <- Filter(function(x) all(x$X %in% compvar) & all(x$Y %in% compvar) & all(x$Z %in% compvar),
-                  tests)
-  comptestsres <- dagitty::localTests(x=mod,data=compdata,tests=comptests,type="cis.pillai",abbreviate.names=FALSE)
-  result1 <- paste("The proposed directed acyclic graph (DAG) implies the following conditional independencies (where, for example, 'X _||_ Y | Z' should be read as 'X is independent of Y conditional on Z'). Note that variable names are abbreviated: \n \n",
-                   paste0(utils::capture.output(tests), prefix="\n",collapse = "\n"),collapse = "\n")
-
-  if(nrow(comptestsres)==0){
-    result2 <- paste("None of the fully observed variables are conditionally independent.\nHence, no consistency checks will be performed. \nConsider whether it is valid and possible to explore relationships between partially observed variables using the observed data, e.g. avoiding perfect prediction.",collapse = "\n")
-  }else{
-    result2 <- paste("These (conditional) independence statements are explored below using the canonical correlations approach for mixed data. See ??dagitty::localTests for further details. \nResults are shown for variables that are fully observed in the specified dataset.\nThe null hypothesis is that the stated variables are (conditionally) independent. \n \n",
-                     paste0(gsub(" ", "@",utils::capture.output(comptestsres)),prefix="\n",collapse = "\n"),
-"\n \nInterpretation: A small p-value means the stated variables may not be (conditionally) independent in the specified dataset: your data may not be consistent with the proposed DAG. A large p-value means there is little evidence of inconsistency between your data and the proposed DAG. \n \nNote that there may also be other DAGs which your data are consistent with. Also note that these results assume that relationships between variables are linear. Consider exploring the specification of each relationship in your model. \nAlso consider whether it is valid and possible to explore relationships between partially observed variables using the observed data, e.g. avoiding perfect prediction.",collapse = "\n")
+  #Check whether input DAG is a dagitty object or not
+  if(dagitty::is.dagitty(mdag)){
+    mod <- mdag
+  } else {
+    mod <- dagitty::dagitty(mdag, layout=T)
   }
 
-  result <- paste(result1, "\n", result2, collapse = "\n")
+  tests <- dagitty::impliedConditionalIndependencies(mod)
+  result1 <- paste("The proposed directed acyclic graph (DAG) implies the following pairs of variables are (conditionally) independent
+(where, for example, 'X _||_ Y | Z' should be read as 'X is independent of Y conditional on Z').
+Note that variable names are abbreviated. Consider whether these (conditional) independencies are plausible for your study, and update your DAG accordingly: \n \n",
+                   paste0(utils::capture.output(tests), prefix="\n",collapse = "\n"),collapse = "\n")
+
+  #Explore observed data if supplied
+  if (!is.null(data)) {
+    # Keep fully observed variables from dataset
+    mdlist <- mice::md.pattern(data,plot=FALSE)
+    complist <- mdlist[nrow(mdlist),]
+    compvar <- names(complist[complist==0])
+    compdata <- data[,c(compvar)]
+
+    # Perform cond independence tests on fully observed variables only
+    comptests <- Filter(function(x) all(x$X %in% compvar) & all(x$Y %in% compvar) & all(x$Z %in% compvar),
+                    tests)
+    comptestsres <- dagitty::localTests(x=mod,data=compdata,tests=comptests,type="cis.pillai",abbreviate.names=FALSE)
+
+
+    if(nrow(comptestsres)==0){
+      result2 <- paste("None of the fully observed variables are conditionally independent.\nHence, no consistency checks will be performed. \nConsider whether it is valid and possible to explore relationships between partially observed variables using the observed data, e.g. avoiding perfect prediction.",collapse = "\n")
+    }else{
+      result2 <- paste("These (conditional) independence statements are explored below using the canonical correlations approach for mixed data. See ??dagitty::localTests for further details. \nResults are shown for variables that are fully observed in the specified dataset.\nThe null hypothesis is that the stated variables are (conditionally) independent. \n \n",
+                       paste0(gsub(" ", "@",utils::capture.output(comptestsres)),prefix="\n",collapse = "\n"),
+  "\n \nInterpretation: A small p-value means the stated variables may not be (conditionally) independent in the specified dataset: your data may not be consistent with the proposed DAG. A large p-value means there is little evidence of inconsistency between your data and the proposed DAG. \n \nNote that there may also be other DAGs which your data are consistent with. Also note that these results assume that relationships between variables are linear. Consider exploring the specification of each relationship in your model. \nAlso consider whether it is valid and possible to explore relationships between partially observed variables using the observed data, e.g. avoiding perfect prediction.",collapse = "\n")
+    }
+
+    result <- paste(result1, "\n", result2, collapse = "\n")
+  } else result <- result1
+
   message(paste(gsub("@", " ",strwrap(result)),collapse="\n"))
 }
--- tests/testthat/test-exploreDAG.R
+++ tests/testthat/test-exploreDAG.R
@@ -6,7 +6,7 @@ test_that("exploreDAG correctly identifies both the implied independencies and t
           none of them are testable",
   {
     expect_equal(substr(trimws(paste0(gsub("\n"," ",res1$messages), collapse=" "),
-                               "right"),358,606),
+                               "right"),497,745),
 "None of the fully observed variables are conditionally independent. Hence, no consistency checks will be performed.  Consider whether it is valid and possible to explore relationships between partially observed variables using the observed data, e.g")
   }
 )
@@ -19,9 +19,32 @@ test_that("exploreDAG correctly identifies both the implied independencies and
           the testable subset",
   {
     expect_equal(substr(trimws(paste0(gsub("\n"," ",res2$messages), collapse=" "),
-                               "right"),378,543),
+                               "right"),517,682),
 "These (conditional) independence statements are explored below using the canonical correlations approach for mixed data. See ??dagitty::localTests for further details")
   }
 )
 
+# Check output when a dataset is not supplied
+res3<-evaluate_promise(exploreDAG(mdag="matage -> bmi7 mated -> matage
+        mated -> bmi7 sep_unmeas -> mated bmi7 -> r"))
+#Trim output for test purposes
+test_that("exploreDAG correctly identifies the implied independencies",
+          {
+            expect_equal(substr(trimws(paste0(gsub("\n"," ",res3$messages), collapse=" "),
+                                       "right"),1,334),
+                         "The proposed directed acyclic graph (DAG) implies the following pairs of variables are (conditionally) independent (where, for example, 'X _||_ Y | Z' should be read as 'X is independent of Y conditional on Z'). Note that variable names are abbreviated. Consider whether these (conditional) independencies are plausible for your study")
+          }
+)
 
+# Check output when a dagitty object is supplied
+dag <- dagitty::dagitty("matage -> bmi7 mated -> matage
+        mated -> bmi7 sep_unmeas -> mated bmi7 -> r", layout=T)
+res4<-evaluate_promise(exploreDAG(mdag=dag))
+#Trim output for test purposes
+test_that("exploreDAG correctly identifies the implied independencies when a dagitty object is supplied",
+          {
+            expect_equal(substr(trimws(paste0(gsub("\n"," ",res4$messages), collapse=" "),
+                                       "right"),1,334),
+                         "The proposed directed acyclic graph (DAG) implies the following pairs of variables are (conditionally) independent (where, for example, 'X _||_ Y | Z' should be read as 'X is independent of Y conditional on Z'). Note that variable names are abbreviated. Consider whether these (conditional) independencies are plausible for your study")
+          }
+)
