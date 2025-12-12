4ab829c4c3e762db0b6b9ddac27c26e367b34f58
elliecurnow <elinor.curnow@bristol.ac.uk>
2025-12-12 10:08:18 +0000

    Added by option to descMissData

--- R/descMissData.R
+++ R/descMissData.R
@@ -4,8 +4,8 @@
 #' Each row in the output corresponds to a missing data pattern (1=observed,
 #' 0=missing). The number and percentage of observations is also displayed for
 #' each missing data pattern. The first column indicates the number of missing
-#' data patterns. The second column refers to the analysis model outcome
-#' ('y'), with all other variables ('covs') displayed in subsequent columns.
+#' data patterns. The second column refers to the analysis model outcome ('y'),
+#' with all other variables ('covs') displayed in subsequent columns.
 #' Alternatively, 'y' can be used to display the primary variable of interest,
 #' e.g. 'y' could refer to the exposure, with all other variables listed in
 #' 'covs'.
@@ -13,33 +13,58 @@
 #' @param y The analysis model outcome, specified as a string
 #' @param covs The analysis model covariate(s), specified as a string (space
 #'   delimited)
-#' @param data A data frame containing the specified analysis model outcome and
-#'   covariate(s)
+#' @param data A data frame containing the specified analysis model outcome,
+#'   covariate(s), and if specified, stratification variable(s)
+#' @param by Optional stratification variable(s), which must be a factor, or
+#'   list of factors; if specified, the data are subsetted by the values of the
+#'   factor(s) and missing data patterns are displayed for each subset in turn
 #' @param plot If TRUE, displays a plot using \link[mice]{md.pattern} to
-#' visualise the missing data patterns; use plot = FALSE (the default) to
-#' disable the plot
+#'   visualise the missing data patterns; if stratification variable(s) are
+#'   specified, a separate plot will be displayed for each subset; use plot =
+#'   FALSE (the default) to disable the plot
 #'
 #' @return A summary of the missing data patterns
 #' @export
 #'
 #' @examples
 #' descMissData(y="bmi7", covs="matage mated", data=bmi)
-#' descMissData(y="bmi7", covs="matage mated pregsize bwt", data=bmi, plot=TRUE)
-descMissData <- function(y, covs, data, plot=FALSE) {
+#' descMissData(y="bmi7", covs="matage mated bwt", by="pregsize", data=bmi)
+descMissData <- function(y, covs, data, by=NULL, plot=FALSE) {
   covslist <- unlist(strsplit(covs," "))
-  mdtab <- mice::md.pattern(data[,c(y,covslist)],plot=plot)
 
-  #Only print if dataset is not completely observed - if it is, print output as per md.pattern
-  if (mdtab[nrow(mdtab),ncol(mdtab)] != 0){
-    #reorder so outcome is listed first
-    mdtab2 <- mdtab[1:nrow(mdtab)-1,c(y,covslist)]
-    n <- as.numeric(row.names(mdtab[1:nrow(mdtab)-1,]))
-    pct <-  round(n*100/sum(n))
-    pattern <- 1:(nrow(mdtab)-1)
-    result<-data.frame(pattern,mdtab2,n,pct)
-    row.names(result) <- NULL
-    return(result)
+  if(is.null(by)){
+    mdtab <- list(mice::md.pattern(data[,c(y,covslist)],plot=plot))
+  } else {
+    mdtab <- by(data[,c(y,covslist)],data[,c(by)],
+                function(x) mice::md.pattern(x,plot=plot))
+    names(dimnames(mdtab)) = by
   }
+
+  for (i in 1:length(mdtab)){
+
+    #Removed next condition because summary is useful even if no missing data
+    #if (mdtab[[i]][nrow(mdtab[[i]]),ncol(mdtab[[i]])] != 0){
+    mdtmp <- mdtab[[i]]
+
+    #Summarise
+    n <- as.numeric(row.names(mdtmp)[1:nrow(mdtmp)])
+    pct <-  round(n*100/sum(n,na.rm=TRUE))
+    pattern <- 1:(nrow(mdtmp))
+
+    #Reorder so outcome is listed first
+    mdtmp2 <- mdtmp[,c(y,covslist)]
+
+    #Combine with summary variables
+    mdtmp3 <- cbind(pattern,mdtmp2,n,pct)
+    row.names(mdtmp3) <- NULL
+    mdtab[[i]] <- mdtmp3[1:nrow(mdtmp)-1,]
+  }
+
+  return(mdtab)
+    #result<-data.frame(pattern,mdtmp2,n,pct)
+    #row.names(result) <- NULL
+    #return(result)
 }
+
 
 
--- tests/testthat/test-descMissData.R
+++ tests/testthat/test-descMissData.R
@@ -5,7 +5,17 @@ res1<-evaluate_promise(descMissData(y="bmi7", covs="matage mated", data=bmi))
 test_that("descMissData output is as expected when data are missing",
   {
     expect_equal(trimws(paste0(gsub("\n","",res1$result), collapse=" "),"right"),
-"1:2 c(1, 0) c(1, 1) c(1, 1) c(592, 408) c(59, 41)")
+"c(1, 2, 1, 0, 1, 1, 1, 1, 592, 408, 59, 41)")
   }
 )
 
+# Check output when by is specified
+res2<-evaluate_promise(descMissData(y="bmi7", covs="matage mated", by="pregsize",data=bmi))
+## Plot is not tested
+#There's a trailing blank, but only visible in testing, so just trim for test purposes
+test_that("descMissData output is as expected when 'by' is specified",
+          {
+            expect_equal(trimws(paste0(gsub("\n","",res2$result), collapse=" "),"right"),
+"c(1, 2, 1, 0, 1, 1, 1, 1, 524, 365, 59, 41) c(1, 2, 1, 0, 1, 1, 1, 1, 68, 43, 61, 39)")
+          }
+)
